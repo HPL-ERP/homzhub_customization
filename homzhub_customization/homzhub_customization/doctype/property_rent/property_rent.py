f34b7416a21202f8399728622753e9d5d6512265
frappe <varade.suraj787@gmail.com>
2020-07-08 17:49:20 +0000

    custom fields and rent mngmt docs

---
+++ homzhub_customization/homzhub_customization/doctype/property_rent/property_rent.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Homzhub and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class PropertyRent(Document):
+	pass
