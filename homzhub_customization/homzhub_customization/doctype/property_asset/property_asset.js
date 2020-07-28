4fbf9182a3d4ef3b967e50be58fbc09e46a3c1a3
frappe <varade.suraj787@gmail.com>
2020-07-28 19:57:33 +0000

    custom fields

---
+++ homzhub_customization/homzhub_customization/doctype/property_asset/property_asset.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Homzhub and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Property Asset', {
+	// refresh: function(frm) {
+
+	// }
+});
