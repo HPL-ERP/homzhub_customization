26dd1539c48c18c28534bb8096855fa992256ba1
frappe <varade.suraj787@gmail.com>
2020-09-29 17:20:57 +0000

    utilit billing fields

---
+++ homzhub_customization/homzhub_customization/doctype/billing_data/test_billing_data.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, Homzhub and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestBillingData(unittest.TestCase):
+	pass
